659348eb38ea635c54fc51e195d5a99746a8ac57
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2026-03-31 17:05:26 -0500

    Update

--- src/search_monitoring_tools/otlp/otlp_grpc.py
+++ src/search_monitoring_tools/otlp/otlp_grpc.py
@@ -1,7 +1,25 @@
 class otlp_grpc:
 
-    def __init__(self):
-        pass
-
+    def __init__(self, otlp_host):
+        """Processes input data.
+        :param otlp_host: the name of otlp_host.
+        :param bool verbose: Enable detailed logging, defaults to False.
+        :return: A dictionary of results.
+        :rtype: dict
+        """
+        
+        self.otlp_host = otlp_host
+   
     def get_test(self):
+        """get_test.
+        :return: String of results.
+        :rtype: string
+        """
         return "Ok"
+    
+    def get_otlp_host(self):
+        """Get the name of otlp host.
+        :rtype: string
+        """
+        
+        return self.otlp_host
--- tests/test_packages.py
+++ tests/test_packages.py
@@ -1,23 +1,38 @@
 
 import pytest
-import hey_utils
-from search_monitoring_tools import otlp_grpc
+import search_monitoring_tools
+
+''' import module via packages '''
+''' reinstall the package 
+pip install ./dist/*.whl --force-reinstall
+'''
+# from search_monitoring_tools import otlp_grpc
+
+# import module directly based on the path
+from src.search_monitoring_tools.otlp.otlp_grpc import otlp_grpc
+
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_skip():
     assert 1 != 1
 
 def test_module_func():
-    response = hey_utils.get_test()
+    response = search_monitoring_tools.get_test()
     assert response is not None
     assert response == "ok"
         
     
 def test_gRPC():
     # response = otlp_grpc.otlp_grpc().get_test()
-    response = otlp_grpc().get_test()
+
+    otlp_instance = otlp_grpc("localhost")
+
+    response = otlp_instance.get_test()
     assert response is not None
     assert response == "Ok"
+
+    response = otlp_instance.get_otlp_host()
+    assert response == "localhost"
     
     
     
